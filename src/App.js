0bc120ee3097bf5729162e539e7d14e3821ce2f6
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-25 15:09:29 -0500

    import Routes, Nav, Home, ShowAll, SignIn, SignUp, NewItem, ShowItem and Edit,

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
 import { useState, useEffect } from 'react';
+import {Routes, Route, useNavigate} from "react-router-dom"
+import Nav from "./components/common/Nav"
+import Home from "./pages/Home"
+import ShowAll from './pages/ShowAll';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import NewItem from './pages/NewItem'
+import ShowItem from './pages/ShowItem';
+import Edit from './pages/Edit';
 
 const URL = process.env.REACT_APP_URL
 
 function App() {
-  const [transactions, setTransactions] = useState([])
+  const nav = useNavigate()
+  const userKey = document.cookie.split(';')[0].split("=")[1]
+  const [total, setTotal] = useState(0)
+  const [isLoggedIn, setLoggedIn] = useState(true)
 
-  useEffect(() => {     
+  useEffect(() => {   
     axios
-    .get(`${URL}/budget`)
-    .then((response) => {       
-      setTransactions(response.data);       
-      console.log(response.data, URL);     
-    });
-  }, [])
+    .get(`${URL}/budget`, {params: {userKey}})
+    .then((response) => {
+      if(response.data.info !== "You need to sign in"){
+        setTotal(response.data.reduce((a, b) => {
+          a = a.amount + b.amount
+          return {amount: a}
+        }).amount) 
+      } else {
+        nav('/signin')
+      }
+        
+    })
+    .catch(console.log)
+  }, [total])
+
+
   return (
     <div className="App">
-      {transactions.map(el=> el.source)}
+      <Nav total={total} isLoggedIn={isLoggedIn}/>
+      <Routes>
+        <Route exact path="/" element={<Home setLoggedIn={setLoggedIn} setTotal={setTotal}/>}/>
+        <Route path="/budget" element={<ShowAll setTotal={setTotal}/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/budget/new" element={<NewItem/>}/>
+        <Route path="/budget/edit/:index" element={<Edit/>}/>
+        <Route path="/budget/show/:index" element={<ShowItem/>}/>
+      </Routes>
     </div>
   );
 }

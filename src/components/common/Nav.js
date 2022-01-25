ea59affb0732bafc8d8a4e9ba30b1388f11b57f9
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-25 15:13:27 -0500

    import Link and useNavigate, add handleLogOut to delete cookies

---
+++ src/components/common/Nav.js
@@ -0,0 +1,31 @@
+import {Link, useNavigate} from "react-router-dom"
+
+
+const URL = process.env.REACT_APP_URL
+function Nav({total, isLoggedIn}) {
+    const nav = useNavigate()
+    const handleLogOut = () => {
+        document.cookie = "loginCred= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+        nav('/')
+    }
+
+    return (
+        <div className="App">
+            <Link to="/">
+                Home
+            </Link>
+            <Link to="/budget">
+                Budget
+            </Link>
+            <Link to="/budget/new">
+                New Item
+            </Link>
+             {isLoggedIn && <button onClick={handleLogOut}>
+                LogOut
+            </button>}
+            <div>{total}</div>
+        </div>
+    );
+}
+  
+  export default Nav;

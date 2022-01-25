f440b3c3036b570677023d21c97af905dafb3f06
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-25 15:16:35 -0500

    import NewTransaction

---
+++ src/pages/NewItem.js
@@ -0,0 +1,13 @@
+import NewTransaction from "../components/NewTransaction"
+
+function NewItem() {
+
+    return (
+        <div>
+            <h2>New</h2>
+            <NewTransaction/>
+        </div>
+    )
+}
+
+export default NewItem

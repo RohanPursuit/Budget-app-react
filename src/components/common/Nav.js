5263793494832e8a6d7f14177eb9a2836fe6196d
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-26 01:38:15 -0500

    link style in nav.js

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,27 +1,31 @@
 import {Link, useNavigate} from "react-router-dom"
+import "../../styles/Nav.css"
 
 function Nav({total, isLoggedIn}) {
     const nav = useNavigate()
+
     const handleLogOut = () => {
         document.cookie = "loginCred= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
         nav('/signin')
     }
 
     return (
-        <div className="App">
-            <Link to="/">
-                Home
+        <div className="Nav">
+            <div className="links">
+            <Link className="logo" to="/">
+                💸
             </Link>
-            <Link to="/budget">
-                Budget
+            <Link className="budget" to="/budget">
+                Budget App
             </Link>
-            <Link to="/budget/new">
-                New Item
+            <Link className="new-transaction" to="/budget/new">
+                NEW TRANSACTION
             </Link>
-             {isLoggedIn && <button onClick={handleLogOut}>
+            </div>
+             {isLoggedIn && <button className="login" onClick={handleLogOut}>
                 LogOut
             </button>}
-            <div>{total}</div>
+            <div className="account-total">Back Account Total: <span className={total === 0 ? "isZero" : total < 0 ? "isLess" : "isGreater"}>${total.toLocaleString("en-US")}</span></div>
         </div>
     );
 }
